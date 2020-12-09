2dd67d1a44d63b076449dcaeed9d4b4b1c7f86db
Michele Conti <11929535+mikcnt@users.noreply.github.com>
2020-12-09 15:49:46 +0100

    Add: (simple) parsing

---
+++ fit.py
@@ -0,0 +1,103 @@
+# For plotting
+import numpy as np
+import matplotlib.pyplot as plt
+# For conversion
+from skimage.color import lab2rgb, rgb2lab, rgb2gray
+from skimage import io
+# For everything
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# For our model
+import torchvision.models as models
+from model import ColorizationNet
+from torchvision import datasets, transforms
+from dataset_class import GrayscaleImageFolder
+# For utilities
+import os, shutil, time, argparse
+from utils import AverageMeter, to_rgb
+
+def validate(val_loader, model, criterion, save_images, epoch, use_gpu):
+    model.eval()
+
+    # Prepare value counters and timers
+    batch_time, data_time, losses = AverageMeter(), AverageMeter(), AverageMeter()
+
+    end = time.time()
+    already_saved_images = False
+    for i, (input_gray, input_ab, target) in enumerate(val_loader):
+        data_time.update(time.time() - end)
+
+        # Use GPU
+        if use_gpu: input_gray, input_ab, target = input_gray.cuda(), input_ab.cuda(), target.cuda()
+
+        # Run model and record loss
+        output_ab = model(input_gray) # throw away class predictions
+        loss = criterion(output_ab, input_ab)
+        losses.update(loss.item(), input_gray.size(0))
+
+        # Save images to file
+        if save_images and not already_saved_images:
+            already_saved_images = True
+            for j in range(min(len(output_ab), 10)): # save at most 5 images
+                save_path = {'grayscale': 'outputs/gray/', 'colorized': 'outputs/color/'}
+                save_name = 'img-{}-epoch-{}.jpg'.format(i * val_loader.batch_size + j, epoch)
+                to_rgb(input_gray[j].cpu(), ab_input=output_ab[j].detach().cpu(), save_path=save_path, save_name=save_name)
+
+        # Record time to do forward passes and save images
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # Print model accuracy -- in the code below, val refers to both value and validation
+        if i % 25 == 0:
+            print('Validate: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(i, len(val_loader), batch_time=batch_time, loss=losses))
+
+    print('Finished validation.')
+    return losses.avg
+
+
+def train(train_loader, model, criterion, optimizer, epoch, use_gpu):
+    print('Starting training epoch {}'.format(epoch))
+    model.train()
+    
+    # Prepare value counters and timers
+    batch_time, data_time, losses = AverageMeter(), AverageMeter(), AverageMeter()
+
+    end = time.time()
+    for i, (input_gray, input_ab, target) in enumerate(train_loader):
+        
+        # Use GPU if available
+        if use_gpu: input_gray, input_ab, target = input_gray.cuda(), input_ab.cuda(), target.cuda()
+
+        # Record time to load data (above)
+        data_time.update(time.time() - end)
+
+        # Run forward pass
+        output_ab = model(input_gray) 
+        loss = criterion(output_ab, input_ab) 
+        losses.update(loss.item(), input_gray.size(0))
+
+        # Compute gradient and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # Record time to do forward and backward passes
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # Print model accuracy -- in the code below, val refers to value, not validation
+        if i % 25 == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, 
+                                                                  i, 
+                                                                  len(train_loader), 
+                                                                  batch_time=batch_time,
+                                                                  data_time=data_time, 
+                                                                  loss=losses)) 
+
+    print('Finished training epoch {}'.format(epoch))
--- main.py
+++ main.py
@@ -13,140 +13,79 @@ import torchvision.models as models
 from model import ColorizationNet
 from torchvision import datasets, transforms
 from dataset_class import GrayscaleImageFolder
+from fit import train, validate
 # For utilities
-import os, shutil, time
+import os, shutil, time, argparse
 from utils import AverageMeter, to_rgb
 
+# Parse arguments and prepare program
+parser = argparse.ArgumentParser(description='Training and Using ColorizationNet')
+parser.add_argument('--resume', default='', type=str, metavar='PATH', help='path to .pth file checkpoint (default: none)')
+# parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true', help='use this flag to validate without training')
 
-# Check if GPU is available
-use_gpu = torch.cuda.is_available()
+def main():
+    global args, best_losses, use_gpu
+    args = parser.parse_args()
+    print('Arguments: {}'.format(args))
 
-# Model definition
-model = ColorizationNet()
+    # Check if GPU is available
+    use_gpu = torch.cuda.is_available()
 
-# Loss and optimizer definition
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=0.0)
+    # Model definition
+    model = ColorizationNet()
 
-# Training data
-train_transforms = transforms.Compose([transforms.RandomResizedCrop(224), transforms.RandomHorizontalFlip()])
-train_imagefolder = GrayscaleImageFolder('data/train', train_transforms)
-train_loader = torch.utils.data.DataLoader(train_imagefolder, batch_size=16, shuffle=True)
+    # Loss and optimizer definition
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2, weight_decay=0.0)
 
-# Validation data
-val_transforms = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224)])
-val_imagefolder = GrayscaleImageFolder('data/val', val_transforms)
-val_loader = torch.utils.data.DataLoader(val_imagefolder, batch_size=16, shuffle=False)
+    # Resume training if checkpoint path is given
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print('Loading checkpoint {}...'.format(args.resume))
+            checkpoint = torch.load(args.resume, map_location=lambda storage, loc: storage)# if use_gpu else torch.load(args.resume, map_location=lambda storage, loc: storage)
+            # args.start_epoch = checkpoint['epoch']
+            # best_losses = checkpoint['best_losses']
+            model.load_state_dict(checkpoint)
+            # optimizer.load_state_dict(checkpoint['optimizer'])
+            print('Finished loading checkpoint.')# Resuming from epoch {}'.format(checkpoint['epoch']))
+        else:
+            print('Checkpoint filepath incorrect.')
+            return
+
+    # Training data
+    train_transforms = transforms.Compose([transforms.RandomResizedCrop(224), transforms.RandomHorizontalFlip()])
+    train_imagefolder = GrayscaleImageFolder('data/train', train_transforms)
+    train_loader = torch.utils.data.DataLoader(train_imagefolder, batch_size=16, shuffle=True)
+
+    # Validation data
+    val_transforms = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224)])
+    val_imagefolder = GrayscaleImageFolder('data/val', val_transforms)
+    val_loader = torch.utils.data.DataLoader(val_imagefolder, batch_size=16, shuffle=False)
+            
+    # Move model and loss function to GPU
+    if use_gpu: 
+        criterion = criterion.cuda()
+        model = model.cuda()
         
-        
-def validate(val_loader, model, criterion, save_images, epoch):
-    model.eval()
+    # Make folders and set parameters
+    os.makedirs('outputs/color', exist_ok=True)
+    os.makedirs('outputs/gray', exist_ok=True)
+    os.makedirs('checkpoints', exist_ok=True)
+    save_images = True
+    best_losses = 1e10
+    epochs = 100
 
-    # Prepare value counters and timers
-    batch_time, data_time, losses = AverageMeter(), AverageMeter(), AverageMeter()
-
-    end = time.time()
-    already_saved_images = False
-    for i, (input_gray, input_ab, target) in enumerate(val_loader):
-        data_time.update(time.time() - end)
-
-        # Use GPU
-        if use_gpu: input_gray, input_ab, target = input_gray.cuda(), input_ab.cuda(), target.cuda()
-
-        # Run model and record loss
-        output_ab = model(input_gray) # throw away class predictions
-        loss = criterion(output_ab, input_ab)
-        losses.update(loss.item(), input_gray.size(0))
-
-        # Save images to file
-        if save_images and not already_saved_images:
-            already_saved_images = True
-            for j in range(min(len(output_ab), 10)): # save at most 5 images
-                save_path = {'grayscale': 'outputs/gray/', 'colorized': 'outputs/color/'}
-                save_name = 'img-{}-epoch-{}.jpg'.format(i * val_loader.batch_size + j, epoch)
-                to_rgb(input_gray[j].cpu(), ab_input=output_ab[j].detach().cpu(), save_path=save_path, save_name=save_name)
-
-        # Record time to do forward passes and save images
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        # Print model accuracy -- in the code below, val refers to both value and validation
-        if i % 25 == 0:
-            print('Validate: [{0}/{1}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(i, len(val_loader), batch_time=batch_time, loss=losses))
-
-    print('Finished validation.')
-    return losses.avg
-
-
-def train(train_loader, model, criterion, optimizer, epoch):
-    print('Starting training epoch {}'.format(epoch))
-    model.train()
-    
-    # Prepare value counters and timers
-    batch_time, data_time, losses = AverageMeter(), AverageMeter(), AverageMeter()
-
-    end = time.time()
-    for i, (input_gray, input_ab, target) in enumerate(train_loader):
-        
-        # Use GPU if available
-        if use_gpu: input_gray, input_ab, target = input_gray.cuda(), input_ab.cuda(), target.cuda()
-
-        # Record time to load data (above)
-        data_time.update(time.time() - end)
-
-        # Run forward pass
-        output_ab = model(input_gray) 
-        loss = criterion(output_ab, input_ab) 
-        losses.update(loss.item(), input_gray.size(0))
-
-        # Compute gradient and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # Record time to do forward and backward passes
-        batch_time.update(time.time() - end)
-        end = time.time()
-
-        # Print model accuracy -- in the code below, val refers to value, not validation
-        if i % 25 == 0:
-            print('Epoch: [{0}][{1}/{2}]\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(epoch, 
-                                                                  i, 
-                                                                  len(train_loader), 
-                                                                  batch_time=batch_time,
-                                                                  data_time=data_time, 
-                                                                  loss=losses)) 
-
-    print('Finished training epoch {}'.format(epoch))
-    
-    
-# Move model and loss function to GPU
-if use_gpu: 
-    criterion = criterion.cuda()
-    model = model.cuda()
-    
-    
-# Make folders and set parameters
-os.makedirs('outputs/color', exist_ok=True)
-os.makedirs('outputs/gray', exist_ok=True)
-os.makedirs('checkpoints', exist_ok=True)
-save_images = True
-best_losses = 1e10
-epochs = 100
-
-
-# Train model
-for epoch in range(epochs):
-    # Train for one epoch, then validate
-    train(train_loader, model, criterion, optimizer, epoch)
-    with torch.no_grad():
-        losses = validate(val_loader, model, criterion, save_images, epoch)
-    # Save checkpoint and replace old best model if current model is better
-    if losses < best_losses:
-        best_losses = losses
-        torch.save(model.state_dict(), 'checkpoints/model-epoch-{}-losses-{:.3f}.pth'.format(epoch+1,losses))
+    # Train model
+    for epoch in range(epochs):
+        # Train for one epoch, then validate
+        train(train_loader, model, criterion, optimizer, epoch, use_gpu)
+        with torch.no_grad():
+            losses = validate(val_loader, model, criterion, save_images, epoch, use_gpu)
+        # Save checkpoint and replace old best model if current model is better
+        if losses < best_losses:
+            best_losses = losses
+            torch.save(model.state_dict(), 'checkpoints/model-epoch-{}-losses-{:.3f}.pth'.format(epoch+1,losses))
+            
+            
+if __name__ == '__main__':
+    main()
