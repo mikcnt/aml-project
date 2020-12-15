9823d924986223f87ab797c297ccacd98dbdbaad
msterpa87 <marcosterpa1@gmail.com>
2020-12-15 12:39:50 +0100

    crossentropy

--- utils.py
+++ utils.py
@@ -3,99 +3,84 @@ import torch
 import matplotlib.pyplot as plt
 from skimage.color import lab2rgb, rgb2lab, rgb2gray
 import sklearn.neighbors as nn
-from scipy.signal import gaussian, convolve
 
 # equations numbers in the following refer to Colorful Image Colorization, Zhang et al.
 # https://arxiv.org/abs/1603.08511
 
-# Load the color prior factor that encourages rare colors
-pts_hull = np.load("data/pts_in_hull.npy")
-q = 313
 
-# parameters for smoothed soft-encoding
-n_points = 1000
-sigma = 5
-lambda_ = 0.5
-eps = 1e-5
+class MultimodalClassification(object):
+    def __init__(self):
+        # Load the color prior factor that encourages rare colors
+        self.pts_hull = np.load("data/pts_in_hull.npy")
+        self.p_tilde = np.load('data/prior_factor.npy')
+        self.nearest = nn.NearestNeighbors(n_neighbors=5, algorithm='ball_tree')
+        self.nearest.fit(self.pts_hull)
 
+        # parameters for smoothed soft-encoding
+        self.n_points = 1000
+        self.sigma = 5
+        self.lambda_ = 0.5
+        self.eps = 1e-5
+        self.q = 313
 
-def multinomial_cross_entropy(z_pred, z_true):
-    p_tilde = np.load('prior_prob.npy')
-    smoothed_normalized = compute_smoothed(z_true)
-    q_stars = smoothed_normalized.argmax(axis=1)
-    w = 1 / (lambda_ * p_tilde + lambda_ / q)
+    def multinomial_cross_entropy(self, z_pred, z_true):
+        smoothed_normalized = self.compute_smoothed(z_true)
+        q_star = smoothed_normalized.argmax(axis=1)
 
-    # normalizing s.t. E[w] = 1
-    total = p_tilde @ w
-    p_tilde = p_tilde / total
+        # rebalancing weighting term
+        weight = self.p_tilde[q_star]
+        z_pred_shifted = z_pred - z_pred.min(axis=0)
+        z_pred_shifted = z_pred_shifted + self.eps
+        z_pred_shifted = z_pred_shifted / z_pred_shifted.sum(axis=0)
+        z_pred_shifted = z_pred_shifted.reshape(-1, self.q)
+        loss = (smoothed_normalized * np.log(z_pred_shifted)).sum(axis=1)
+        loss = - loss * weight
 
-    # rebalancing weighting term
-    weight = w[q_stars]
-    z_pred_shifted = z_pred - z_pred.min(axis=0)
-    z_pred_shifted = z_pred_shifted + eps
-    z_pred_shifted = z_pred_shifted / z_pred_shifted.sum(axis=0)
-    z_pred_shifted = z_pred_shifted.reshape(-1, q)
-    v = (smoothed_normalized * np.log(z_pred_shifted)).sum(axis=1)
-    v = - v * weight
-    return v.sum()
+        return loss.sum()
 
+    def compute_prior_prob(self, image_ab):
+        """
+        image_ab: numpy of shape (2, 224, 224)
+        """
+        # flattened array of shape (224*224, 2)
+        image_a = image_ab[0, :, :].ravel()
+        image_b = image_ab[1, :, :].ravel()
+        image_ab = np.vstack((image_a, image_b)).T
 
-def compute_prior_prob(image_ab):
-    """
-    image_ab: numpy of shape (2, 224, 224)
-    """
-    nearest = nn.NearestNeighbors(n_neighbors=5, algorithm='ball_tree')
-    nearest.fit(pts_hull)
+        # distances and indices of the 5 nearest neighbour of the true image ab channel
+        dists, ind = self.nearest.kneighbors(image_ab)
 
-    # flattened array of shape (224*224, 2)
-    image_a = image_ab[0, :, :].ravel()
-    image_b = image_ab[1, :, :].ravel()
-    image_ab = np.vstack((image_a, image_b)).T
+        ind = np.ravel(ind)
+        counts = np.bincount(ind)
+        idxs = np.nonzero(counts)[0]
 
-    # distances and indices of the 5 nearest neighbour of the true image ab channel
-    dists, ind = nearest.kneighbors(image_ab)
+        # create vector of prior probabilities from non zero occurring pixels
+        prior_prob = np.zeros(self.pts_hull.shape[0])
+        prior_prob[idxs] = counts[idxs]
+        prior_prob = prior_prob / (1.0 * counts.sum())
 
-    ind = np.ravel(ind)
-    counts = np.bincount(ind)
-    idxs = np.nonzero(counts)[0]
+        return prior_prob
 
-    # create vector of prior probabilities from non zero occurring pixels
-    prior_prob = np.zeros(pts_hull.shape[0])
-    prior_prob[idxs] = counts[idxs]
-    prior_prob = prior_prob / (1.0 * counts.sum())
+    def compute_smoothed(self, z_true):
+        """
+        z_true: numpy of shape (2, 224, 224)
+        """
+        # flattened array of shape (224*224, 2)
+        image_a = z_true[0, :, :].ravel()
+        image_b = z_true[1, :, :].ravel()
+        image_ab = np.vstack((image_a, image_b)).T
 
-    return prior_prob
+        # distances and indices of the 5 nearest neighbour of the true image ab channel
+        dists, ind = self.nearest.kneighbors(image_ab)
 
+        wts = np.exp(- dists ** 2) / (2 * self.sigma ** 2)
+        wts = wts / np.sum(wts, axis=1)[:, np.newaxis]
+        z_soft_encoding = np.zeros((image_ab.shape[0], self.q))
 
-def compute_smoothed(image_ab):
-    """
-    image_ab: numpy of shape (2, 224, 224)
-    """
-    # noinspection DuplicatedCode
-    nearest = nn.NearestNeighbors(n_neighbors=5, algorithm='ball_tree')
-    nearest.fit(pts_hull)
+        idx_pts = np.arange(image_ab.shape[0])[:, np.newaxis]
+        z_soft_encoding[idx_pts, ind] = wts
 
-    # flattened array of shape (224*224, 2)
-    image_a = image_ab[0, :, :].ravel()
-    image_b = image_ab[1, :, :].ravel()
-    image_ab = np.vstack((image_a, image_b)).T
-
-    # distances and indices of the 5 nearest neighbour of the true image ab channel
-    dists, ind = nearest.kneighbors(image_ab)
-
-    n = dists.shape[0]
-    z_soft_enc = np.zeros((n, q))
-
-    # create soft encoding vector with 0s everywhere but the 5-nearest pixels
-    for i in range(n):
-        z_soft_enc[i][ind[i]] = dists[i]
-
-    # gaussian smoothing
-    window = gaussian(n_points, sigma)
-    smoothed = np.array([convolve(x, window, mode='same') for x in z_soft_enc])
-    smoothed_normalized = smoothed / smoothed.sum(axis=1).reshape(-1, 1)
-
-    return smoothed_normalized
+        return z_soft_encoding
 
 
 class AverageMeter(object):
@@ -115,13 +100,13 @@ class AverageMeter(object):
 def to_rgb(grayscale_input, ab_input, save_path=None, save_name=None):
     '''Show/save rgb image from grayscale and ab channels
          Input save_path in the form {'grayscale': '/path/', 'colorized': '/path/'}'''
-    plt.clf() # clear matplotlib 
-    color_image = torch.cat((grayscale_input, ab_input), 0).numpy() # combine channels
-    color_image = color_image.transpose((1, 2, 0))    # rescale for matplotlib
+    plt.clf()  # clear matplotlib
+    color_image = torch.cat((grayscale_input, ab_input), 0).numpy()  # combine channels
+    color_image = color_image.transpose((1, 2, 0))  # rescale for matplotlib
     color_image[:, :, 0:1] = color_image[:, :, 0:1] * 100
-    color_image[:, :, 1:3] = color_image[:, :, 1:3] * 255 - 128     
+    color_image[:, :, 1:3] = color_image[:, :, 1:3] * 255 - 128
     color_image = lab2rgb(color_image.astype(np.float64))
     grayscale_input = grayscale_input.squeeze().numpy()
-    if save_path is not None and save_name is not None: 
+    if save_path is not None and save_name is not None:
         plt.imsave(arr=grayscale_input, fname='{}{}'.format(save_path['grayscale'], save_name), cmap='gray')
         plt.imsave(arr=color_image, fname='{}{}'.format(save_path['colorized'], save_name))
